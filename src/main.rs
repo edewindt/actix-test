883b573600655ed1bdb673b5c0ab07d1296ab211
edewindt <edewindt27@outlook.com>
2023-02-28 21:42:37 -0600

    added logger middleware

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
+use actix_web::middleware::Logger;
+use env_logger::Env;
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -16,8 +17,10 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     HttpServer::new(|| {
         App::new()
+        .wrap(Logger::default())
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
